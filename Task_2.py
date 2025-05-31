cfd15bf5761d00394cd4223266b509af20abd917
yash0wiz <141996224+yash0wiz@users.noreply.github.com>
2025-05-31 17:39:16 +0530

    Create Task_2.py

---
+++ Task_2.py
@@ -0,0 +1,18 @@
+
+'''
+Task 2: Demonstrate List Slicing 
+Problem Statement: Write a Python program that:
+1.   Creates a list of numbers from 1 to 10.
+2.   Extracts the first five elements from the list.
+3.   Reverses these extracted elements.
+4.   Prints both the extracted list and the reversed list
+'''
+
+My_List=[1,2,3,4,5,6,7,8,9]
+print("Original list: ",My_List)
+Extract_List=My_List [0:5]
+print("Extracted first five elements: ",Extract_List)
+Extract_List.reverse()
+print("Reversed extracted elements: ",Extract_List)
+
+
